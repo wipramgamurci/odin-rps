05d0a8f0703371449149fb0dc69e73fc8d0c29f1
Wipram Gamurci <wipramgamurci@gmail.com>
2024-01-04 20:49:31 +0800

    Create getComputerChoice
    create function to pick rock, paper, or scissors randomly

---
+++ script.js
@@ -0,0 +1,14 @@
+// console.log("Hello, World!");
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber == 0) {
+    return "rock";
+  } else if (randomNumber == 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
+
+console.log(getComputerChoice());
