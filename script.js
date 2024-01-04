acedd8f8173ad15ed92b46069022e25a8d0b449a
Wipram Gamurci <wipramgamurci@gmail.com>
2024-01-05 01:39:29 +0800

    Create playRound function
    Create playRound function for user to play the round of rps

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 // console.log("Hello, World!");
 
 const getComputerChoice = () => {
-  const randomNumber = Math.floor(Math.random() * 3);
-  if (randomNumber == 0) {
+  const choices = Math.floor(Math.random() * 3);
+  if (choices == 0) {
     return "rock";
-  } else if (randomNumber == 1) {
+  } else if (choices == 1) {
     return "paper";
   } else {
     return "scissors";
   }
 };
 
-console.log(getComputerChoice());
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("rock, paper, or scissors?").toLowerCase();
+
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection == computerSelection) {
+    return "It's a Tie!";
+  } else if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "rock")
+  ) {
+    return `You lose, ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return `You win, ${playerSelection} beats ${computerSelection}`;
+  }
+};
+
+console.log(computerSelection);
+console.log(playerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
