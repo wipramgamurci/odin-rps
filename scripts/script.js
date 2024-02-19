a571ea2e598f11d1da13b710c73b6c2909f2300b
Wipram Gamurci <wipramgamurci@gmail.com>
2024-02-19 16:41:38 +0800

    Create UI - create ui - refactor code

--- scripts/script.js
+++ scripts/script.js
@@ -11,36 +11,75 @@ const getComputerChoice = () => {
 
 let playerScore = 0;
 let computerScore = 0;
+let isGameOver = false;
 
 const playRound = (playerSelection, computerSelection) => {
   if (playerSelection == computerSelection) {
-    return "It's a Tie!";
+    resultText.textContent = "It's a tie!";
   } else if (
     (playerSelection == "rock" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "rock")
   ) {
     computerScore++;
-    return `You lose, ${computerSelection} beats ${playerSelection}`;
+    resultText.textContent = `You lose, ${computerSelection} beats ${playerSelection}`;
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
   } else {
     playerScore++;
-    return `You win, ${playerSelection} beats ${computerSelection}`;
+    resultText.textContent = `You win, ${playerSelection} beats ${computerSelection}`;
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+  }
+
+  if (playerScore === 5) {
+    resultText.textContent = "You Win!!!";
+    isGameOver = true;
+  } else if (computerScore === 5) {
+    resultText.textContent = "You Lose :(";
+    isGameOver = true;
+  }
+
+  if (isGameOver === true) {
+    playerSelectionButton.forEach((button) => (button.disabled = true));
+    const resetButtonWrapper = document.querySelector("#reset");
+    const resetButton = document.createElement("button");
+    resetButton.setAttribute("id", "resetButton");
+    resetButton.textContent = "Reset";
+    resetButtonWrapper.appendChild(resetButton);
+    resetButton.addEventListener("click", () => {
+      location.reload();
+    });
   }
 };
 
-const game = () => {
-  for (let i = 0; i < 5; i++) {
+const playerSelectionButton = document.querySelectorAll("button");
+
+playerSelectionButton.forEach((button) => {
+  button.addEventListener("click", () => {
     const computerSelection = getComputerChoice();
-    const playerSelection = prompt("rock, paper, or scissors?").toLowerCase();
-    console.log(playRound(playerSelection, computerSelection));
-  }
-  if (playerScore < computerScore) {
-    console.log(`You Lose, ${playerScore} : ${computerScore}`);
-  } else if (playerScore == computerScore) {
-    console.log(`It's a Tie, ${playerScore} : ${computerScore}`);
-  } else {
-    console.log(`You Win, ${playerScore} : ${computerScore}`);
-  }
-};
+    const playerSelection = button.id;
+    playRound(playerSelection, computerSelection);
+  });
+});
 
-console.log(game());
+const resultText = document.querySelector("#result");
+const playerScoreText = document.querySelector("#playerScore");
+const computerScoreText = document.querySelector("#computerScore");
+
+// const game = () => {
+//   for (let i = 0; i < 5; i++) {
+//     const computerSelection = getComputerChoice();
+//     const playerSelection = prompt("rock, paper, or scissors?").toLowerCase();
+//     console.log(playRound(playerSelection, computerSelection));
+//   }
+//   if (playerScore < computerScore) {
+//     console.log(`You Lose, ${playerScore} : ${computerScore}`);
+//   } else if (playerScore == computerScore) {
+//     console.log(`It's a Tie, ${playerScore} : ${computerScore}`);
+//   } else {
+//     console.log(`You Win, ${playerScore} : ${computerScore}`);
+//   }
+// };
+
+// console.log(game());
